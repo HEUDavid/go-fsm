706f3d403e7e947d98be70046a2daa81326bd6d9
David Xiang <admin@mdavid.cn>
2024-07-09 23:57:18 +0800

    code review

--- pkg/metadata/view.go
+++ pkg/metadata/view.go
@@ -4,52 +4,59 @@ import (
 	"fmt"
 )
 
-type IState interface {
+type IState[ExtData ExtDataEntity] interface {
 	GetName() string
 	IsFinalState() bool
-	Handle(task *Task[ExtDataEntity]) error
+	Handle(task *Task[ExtData]) error
 }
 
-type State struct {
-	IState
-	Name    string
-	IsFinal bool
+type State[ExtData ExtDataEntity] struct {
+	Name     string
+	IsFinal  bool
+	ReHandle func(task *Task[ExtData]) error
 }
 
-func (s State) GetName() string                        { return s.Name }
-func (s State) IsFinalState() bool                     { return s.IsFinal }
-func (s State) Handle(task *Task[ExtDataEntity]) error { return nil }
+func (s State[ExtData]) GetName() string    { return s.Name }
+func (s State[ExtData]) IsFinalState() bool { return s.IsFinal }
+func (s State[ExtData]) Handle(task *Task[ExtData]) error {
+	if s.ReHandle != nil {
+		return s.ReHandle(task)
+	}
 
-type ITransition interface {
+	// TODO implement me
+	panic("implement me")
+}
+
+type ITransition[ExtData ExtDataEntity] interface {
 	GetName() string
 }
 
-type Transition struct {
-	From IState
-	To   IState
+type Transition[ExtData ExtDataEntity] struct {
+	From State[ExtData]
+	To   State[ExtData]
 }
 
-func (t Transition) GetName() string {
+func (t Transition[ExtData]) GetName() string {
 	return fmt.Sprintf("%s->%s", t.From.GetName(), t.To.GetName())
 }
 
-type IFSM interface {
-	GetState(stateName string) (State, bool)
-	GetTransition(fromState, toState string) (*Transition, bool)
+type IFSM[ExtData ExtDataEntity] interface {
+	GetState(stateName string) (State[ExtData], bool)
+	GetTransition(fromState, toState string) (Transition[ExtData], bool)
 }
 
-type FSM struct {
-	InitialState IState
-	States       map[string]IState
-	Transitions  map[string]Transition
+type FSM[ExtData ExtDataEntity] struct {
+	InitialState State[ExtData]
+	States       map[string]State[ExtData]
+	Transitions  map[string]Transition[ExtData]
 }
 
-func (f FSM) GetState(stateName string) (IState, bool) {
+func (f FSM[ExtData]) GetState(stateName string) (State[ExtData], bool) {
 	state, exist := f.States[stateName]
 	return state, exist
 }
 
-func (f FSM) GetTransition(fromState, toState string) (Transition, bool) {
-	transition, exist := f.Transitions[fmt.Sprintf("%s->%s", fromState, toState)]
+func (f FSM[ExtData]) GetTransition(from, to string) (Transition[ExtData], bool) {
+	transition, exist := f.Transitions[fmt.Sprintf("%s->%s", from, to)]
 	return transition, exist
 }
