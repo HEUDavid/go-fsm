d4ceea4ed3333052a3703f5549eeef4d4031714d
David Xiang <admin@mdavid.cn>
2024-07-10 09:50:56 +0800

    code review

--- pkg/metadata/metadata.go
+++ pkg/metadata/metadata.go
@@ -6,21 +6,21 @@ import (
 )
 
 type Models struct {
-	ExtDataModel       schema.Tabler
+	DataModel          schema.Tabler
 	TaskModel          schema.Tabler
 	UniqueRequestModel schema.Tabler
-	TaskFlowModel      schema.Tabler // TaskFlow and ExtDataFlow is optional, if not set,
-	ExtDataFlowModel   schema.Tabler // no flow records will be kept
+	TaskFlowModel      schema.Tabler // TaskFlow and DataFlow is optional, if not set,
+	DataFlowModel      schema.Tabler // no flow records will be kept
 }
 
-// ExtDataEntity is ext_data table which has all business columns
-type ExtDataEntity interface {
+// DataEntity is data table which has all business columns
+type DataEntity interface {
 	schema.Tabler
 	SetTaskID(string)
 }
 
 // Task is the main table, maintaining state, driven execution
-type Task[ExtData ExtDataEntity] struct {
+type Task[Data DataEntity] struct {
 	ID         string    `gorm:"primaryKey;column:id;type:char(32);not null"`
 	RequestID  string    `gorm:"unique;column:request_id;type:char(32);not null;comment:'初始请求ID'"` // 初始请求ID
 	Type       string    `gorm:"column:type;type:varchar(128);not null;comment:'业务类型'"`            // 业务类型
@@ -29,36 +29,36 @@ type Task[ExtData ExtDataEntity] struct {
 	CreateTime time.Time `gorm:"column:create_time;type:timestamp;not null;default:current_timestamp()"`
 	UpdateTime time.Time `gorm:"column:update_time;type:timestamp;not null;default:current_timestamp()"`
 
-	ExtData       ExtData  `gorm:"-"`
-	SelectColumns []string `gorm:"-"` // ExtData: 显示声明需要更新的列，一般是zero列，其他non-zero列会自动更新不必声明
-	OmitColumns   []string `gorm:"-"` // ExtData: 显示申明需要忽略更新的列
+	Data          Data     `gorm:"-"` // Data: 自定义数据表
+	SelectColumns []string `gorm:"-"` // Data: 显示声明需要更新的列，一般是zero列，其他non-zero列会自动更新不必显示声明
+	OmitColumns   []string `gorm:"-"` // Data: 显示声明需要忽略更新的列
 }
 
-func (t *Task[ExtData]) GetExtData() *ExtData {
-	return &t.ExtData
+func (t *Task[Data]) GetData() *Data {
+	return &t.Data
 }
 
-func (t *Task[ExtData]) SetExtData(extData ExtData) {
-	t.ExtData = extData
+func (t *Task[Data]) SetData(Data Data) {
+	t.Data = Data
 }
 
-func (t *Task[ExtData]) SetTaskID(taskID string) {
+func (t *Task[Data]) SetTaskID(taskID string) {
 	t.ID = taskID
-	t.ExtData.SetTaskID(taskID)
+	t.Data.SetTaskID(taskID)
 }
 
 // SetSelectColumns 指定要更新的列(即使是零值)
-func (t *Task[ExtData]) SetSelectColumns(columns []string) {
+func (t *Task[Data]) SetSelectColumns(columns []string) {
 	t.SelectColumns = columns
 }
 
 // SetOmitColumns 指定要忽略的列
-func (t *Task[ExtData]) SetOmitColumns(columns []string) {
+func (t *Task[Data]) SetOmitColumns(columns []string) {
 	t.OmitColumns = columns
 }
 
-func GenTaskInstance[ExtData ExtDataEntity](requestID, taskID string, extData ExtData) *Task[ExtData] {
-	task := &Task[ExtData]{ExtData: extData}
+func GenTaskInstance[Data DataEntity](requestID, taskID string, data Data) *Task[Data] {
+	task := &Task[Data]{Data: data}
 	task.RequestID = requestID
 	if taskID != "" {
 		task.SetTaskID(taskID)
