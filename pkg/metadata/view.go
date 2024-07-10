7a23f44059db1d76758866d28575cae17564e0ef
David Xiang <admin@mdavid.cn>
2024-07-10 10:06:38 +0800

    code review

--- pkg/metadata/view.go
+++ pkg/metadata/view.go
@@ -4,91 +4,89 @@ import (
 	"fmt"
 )
 
-type IState[ExtData ExtDataEntity] interface {
+type IState[Data DataEntity] interface {
 	GetName() string
 	IsFinalState() bool
-	Handle(task *Task[ExtData]) error
+	Handle(task *Task[Data]) error
 }
 
-type State[ExtData ExtDataEntity] struct {
+type State[Data DataEntity] struct {
 	Name    string
 	IsFinal bool
-	Handler func(task *Task[ExtData]) error
+	Handler func(task *Task[Data]) error
 }
 
-func (s State[ExtData]) GetName() string    { return s.Name }
-func (s State[ExtData]) IsFinalState() bool { return s.IsFinal }
-func (s State[ExtData]) Handle(task *Task[ExtData]) error {
+func (s State[Data]) GetName() string    { return s.Name }
+func (s State[Data]) IsFinalState() bool { return s.IsFinal }
+func (s State[Data]) Handle(task *Task[Data]) error {
 	if s.Handler != nil {
 		return s.Handler(task)
 	}
 
 	// TODO implement me
-	panic("implement me")
+	panic("[FSM] implement me")
 }
 
-func GenState[ExtData ExtDataEntity](name string, isFinal bool, handler func(task *Task[ExtData]) error) State[ExtData] {
-	return State[ExtData]{
-		Name:    name,
-		IsFinal: isFinal,
-		Handler: handler,
-	}
+func GenState[Data DataEntity](name string, isFinal bool, handler func(task *Task[Data]) error) State[Data] {
+	return State[Data]{name, isFinal, handler}
 }
 
-type ITransition[ExtData ExtDataEntity] interface {
+type ITransition[Data DataEntity] interface {
 	GetName() string
 }
 
-type Transition[ExtData ExtDataEntity] struct {
-	From State[ExtData]
-	To   State[ExtData]
+type Transition[Data DataEntity] struct {
+	From State[Data]
+	To   State[Data]
 }
 
-func (t Transition[ExtData]) GetName() string {
+func (t Transition[Data]) GetName() string {
 	return fmt.Sprintf("%s->%s", t.From.GetName(), t.To.GetName())
 }
 
-func GenTransition[ExtData ExtDataEntity](from, to State[ExtData]) Transition[ExtData] {
-	return Transition[ExtData]{from, to}
+func GenTransition[Data DataEntity](from, to State[Data]) Transition[Data] {
+	return Transition[Data]{from, to}
 }
 
-type IFSM[ExtData ExtDataEntity] interface {
-	GetState(stateName string) (State[ExtData], bool)
-	GetTransition(fromState, toState string) (Transition[ExtData], bool)
+type IFSM[Data DataEntity] interface {
+	RegisterState(states ...State[Data])
+	RegisterTransition(transitions ...Transition[Data])
+	GetState(state string) (State[Data], bool)
+	GetTransition(fromState, toState string) (Transition[Data], bool)
 }
 
-type FSM[ExtData ExtDataEntity] struct {
-	InitialState State[ExtData]
-	States       map[string]State[ExtData]
-	Transitions  map[string]Transition[ExtData]
+type FSM[Data DataEntity] struct {
+	InitialState State[Data]
+	States       map[string]State[Data]
+	Transitions  map[string]Transition[Data]
 }
 
-func (f *FSM[ExtData]) GetState(stateName string) (State[ExtData], bool) {
-	state, exist := f.States[stateName]
-	return state, exist
+func (f *FSM[Data]) GetState(state string) (State[Data], bool) {
+	s, exist := f.States[state]
+	return s, exist
 }
 
-func (f *FSM[ExtData]) GetTransition(from, to string) (Transition[ExtData], bool) {
-	transition, exist := f.Transitions[fmt.Sprintf("%s->%s", from, to)]
-	return transition, exist
+func (f *FSM[Data]) GetTransition(fromState, toState string) (Transition[Data], bool) {
+	t, exist := f.Transitions[fmt.Sprintf("%s->%s", fromState, toState)]
+	return t, exist
 }
 
-func (f *FSM[ExtData]) RegisterState(states ...State[ExtData]) {
+func (f *FSM[Data]) RegisterState(states ...State[Data]) {
 	for _, state := range states {
 		f.States[state.GetName()] = state
 	}
 }
 
-func (f *FSM[ExtData]) RegisterTransition(transitions ...Transition[ExtData]) {
+func (f *FSM[Data]) RegisterTransition(transitions ...Transition[Data]) {
 	for _, transition := range transitions {
 		f.Transitions[transition.GetName()] = transition
 	}
 }
 
-func GenFSM[ExtData ExtDataEntity](Initial State[ExtData]) FSM[ExtData] {
-	return FSM[ExtData]{
-		InitialState: Initial,
-		States:       map[string]State[ExtData]{},
-		Transitions:  map[string]Transition[ExtData]{},
+func GenFSM[Data DataEntity](initial State[Data]) FSM[Data] {
+	return FSM[Data]{
+		InitialState: initial,
+		States:       map[string]State[Data]{},
+		Transitions:  map[string]Transition[Data]{},
 	}
 }
