ce359baee11983d02cf05396e17d544627af5806
David Xiang <admin@mdavid.cn>
2024-07-10 17:25:11 +0800

    code review

--- pkg/metadata/metadata.go
+++ pkg/metadata/metadata.go
@@ -22,16 +22,16 @@ type DataEntity interface {
 // Task is the main table, maintaining state, driven execution
 type Task[Data DataEntity] struct {
 	ID         string    `gorm:"primaryKey;column:id;type:char(32);not null"`
-	RequestID  string    `gorm:"unique;column:request_id;type:char(32);not null;comment:'初始请求ID'"` // 初始请求ID
-	Type       string    `gorm:"column:type;type:varchar(128);not null;comment:'业务类型'"`            // 业务类型
-	State      string    `gorm:"column:state;type:varchar(128);not null;comment:'任务状态'"`           // 任务状态
+	RequestID  string    `gorm:"unique;column:request_id;type:char(32);not null;comment:'初始请求ID'"`
+	Type       string    `gorm:"column:type;type:varchar(128);not null;comment:'业务类型'"`
+	State      string    `gorm:"column:state;type:varchar(128);not null;comment:'任务状态'"`
 	Version    int       `gorm:"column:version;type:int(11);not null;default:1"`
 	CreateTime time.Time `gorm:"column:create_time;type:timestamp;not null;default:current_timestamp()"`
 	UpdateTime time.Time `gorm:"column:update_time;type:timestamp;not null;default:current_timestamp()"`
 
-	Data          Data     `gorm:"-"` // Data: 自定义数据表
-	SelectColumns []string `gorm:"-"` // Data: 显示声明需要更新的列，一般是zero列，其他non-zero列会自动更新不必显示声明
-	OmitColumns   []string `gorm:"-"` // Data: 显示声明需要忽略更新的列
+	Data          Data     `gorm:"-"` // Data: Customized Data Tables
+	SelectColumns []string `gorm:"-"` // Data: Columns to update, including zero values
+	OmitColumns   []string `gorm:"-"` // Data: Columns to be ignored
 }
 
 func (t *Task[Data]) GetData() *Data {
@@ -47,12 +47,12 @@ func (t *Task[Data]) SetTaskID(taskID string) {
 	t.Data.SetTaskID(taskID)
 }
 
-// SetSelectColumns 指定要更新的列(即使是零值)
+// SetSelectColumns Specify columns to update (even if it is zero-valued)
 func (t *Task[Data]) SetSelectColumns(columns []string) {
 	t.SelectColumns = columns
 }
 
-// SetOmitColumns 指定要忽略的列
+// SetOmitColumns Specify columns to ignore
 func (t *Task[Data]) SetOmitColumns(columns []string) {
 	t.OmitColumns = columns
 }
