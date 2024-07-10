d15a5553ef23177526d959989fa404c555760345
David Xiang <admin@mdavid.cn>
2024-07-10 09:12:01 +0800

    code review

--- internal/crud.go
+++ internal/crud.go
@@ -7,17 +7,16 @@ import (
 	"github.com/HEUDavid/go-fsm/pkg/util"
 	"github.com/go-sql-driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
 )
 
-func AddTaskFlow[ExtData ExtDataEntity](tx *gorm.DB, m Models, task *Task[ExtData], queryFirst bool) error {
-	if m.TaskFlowModel == nil || m.ExtDataModel == nil {
+func AddTaskFlow[Data DataEntity](tx *gorm.DB, m Models, task *Task[Data], queryFirst bool) error {
+	if m.TaskFlowModel == nil || m.DataModel == nil {
 		return nil
 	}
 	return nil
 }
 
-func AddUnique[ExtData ExtDataEntity](tx *gorm.DB, uniqueRequestModel schema.Tabler, task *Task[ExtData], needModifyTaskID bool) (bool, error) {
+func AddUnique[Data DataEntity](tx *gorm.DB, m Models, task *Task[Data], needModifyTaskID bool) (bool, error) {
 	uniqueReq := struct {
 		RequestID string
 		TaskID    string
@@ -26,7 +25,7 @@ func AddUnique[ExtData ExtDataEntity](tx *gorm.DB, uniqueRequestModel schema.Tab
 		task.ID,
 	}
 
-	err := tx.Table(uniqueRequestModel.TableName()).Create(&uniqueReq).Error
+	err := tx.Table(m.UniqueRequestModel.TableName()).Create(&uniqueReq).Error
 
 	if err != nil {
 		mysqlErr, ok := err.(*mysql.MySQLError)
@@ -36,7 +35,7 @@ func AddUnique[ExtData ExtDataEntity](tx *gorm.DB, uniqueRequestModel schema.Tab
 		switch mysqlErr.Number {
 		case 1062:
 			if needModifyTaskID { // Use the TaskID recorded in the DB to assign values, making the interface idempotent.
-				if err = tx.Table(uniqueRequestModel.TableName()).Where("request_id = ?", task.RequestID).Scan(&uniqueReq).Error; err != nil {
+				if err = tx.Table(m.UniqueRequestModel.TableName()).Where("request_id = ?", task.RequestID).Scan(&uniqueReq).Error; err != nil {
 					return true, err
 				}
 				task.SetTaskID(uniqueReq.TaskID)
@@ -49,15 +48,15 @@ func AddUnique[ExtData ExtDataEntity](tx *gorm.DB, uniqueRequestModel schema.Tab
 	}
 }
 
-func CreateTaskTx[ExtData ExtDataEntity](c context.Context, db *gorm.DB, m Models, task *Task[ExtData]) error {
+func CreateTaskTx[Data DataEntity](c context.Context, db *gorm.DB, m Models, task *Task[Data]) error {
 	if err := db.Transaction(func(tx *gorm.DB) error { return _createTaskTx(c, tx, m, task) }); err != nil {
 		return err
 	}
 	return nil
 }
 
-func _createTaskTx[ExtData ExtDataEntity](c context.Context, tx *gorm.DB, m Models, task *Task[ExtData]) error {
-	keyConflict, e := AddUnique(tx, m.UniqueRequestModel, task, true)
+func _createTaskTx[Data DataEntity](c context.Context, tx *gorm.DB, m Models, task *Task[Data]) error {
+	keyConflict, e := AddUnique(tx, m, task, true)
 	if e != nil {
 		return e
 	}
@@ -68,7 +67,7 @@ func _createTaskTx[ExtData ExtDataEntity](c context.Context, tx *gorm.DB, m Mode
 	if e = tx.Table(m.TaskModel.TableName()).Create(&task).Error; e != nil {
 		return e
 	}
-	if e = tx.Table(m.ExtDataModel.TableName()).Create(task.GetExtData()).Error; e != nil {
+	if e = tx.Table(m.DataModel.TableName()).Create(task.GetData()).Error; e != nil {
 		return e
 	}
 
@@ -79,43 +78,47 @@ func _createTaskTx[ExtData ExtDataEntity](c context.Context, tx *gorm.DB, m Mode
 	return nil
 }
 
-func QueryTaskTx[ExtData ExtDataEntity](c context.Context, db *gorm.DB, taskModel, extDataModel schema.Tabler, task *Task[ExtData]) error {
-	stmt := db
-	if task.ID != "" {
-		// When the destination object has a primary key value, it will be used to construct the condition
-	} else {
-		stmt = stmt.Where("request_id = ?", task.RequestID)
+func QueryTaskTx[Data DataEntity](c context.Context, db *gorm.DB, m Models, task *Task[Data]) error {
+	_queryTask := func(_tx *gorm.DB) *gorm.DB {
+		q := _tx.Table(m.TaskModel.TableName())
+		if task.ID != "" {
+			// When the destination object has a primary key value, it will be used to construct the condition
+		} else {
+			q = q.Where("request_id = ?", task.RequestID)
+		}
+		return q
 	}
-
-	if err := stmt.Table(taskModel.TableName()).First(task).Error; err != nil {
+	if err := _queryTask(db).First(task).Error; err != nil {
 		return err
 	}
-	if extDataModel != nil {
-		if err := db.Table(extDataModel.TableName()).Where("task_id = ?", task.ID).Find(task.ExtData).Error; err != nil {
-			return err
-		}
+
+	_queryData := func(_tx *gorm.DB) *gorm.DB {
+		return _tx.Table(m.DataModel.TableName()).Where("task_id = ?", task.ID)
+	}
+	if err := _queryData(db).Find(task.GetData()).Error; err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func QueryTaskState(c context.Context, tx *gorm.DB, taskModel schema.Tabler, taskID string) (*string, error) {
+func QueryTaskState(c context.Context, tx *gorm.DB, m Models, taskID string) (*string, error) {
 	var state string
-	if err := tx.Table(taskModel.TableName()).Select("state").Where("id = ?", taskID).Scan(&state).Error; err != nil {
+	if err := tx.Table(m.TaskModel.TableName()).Select("state").Where("id = ?", taskID).Scan(&state).Error; err != nil {
 		return nil, err
 	}
 	return &state, nil
 }
 
-func UpdateTaskTx[ExtData ExtDataEntity](c context.Context, db *gorm.DB, m Models, task *Task[ExtData], fsm FSM[ExtData]) error {
+func UpdateTaskTx[Data DataEntity](c context.Context, db *gorm.DB, m Models, task *Task[Data], fsm FSM[Data]) error {
 	if err := db.Transaction(func(tx *gorm.DB) error { return _updateTaskTx(c, tx, m, task, fsm) }); err != nil {
 		return err
 	}
 	return nil
 }
 
-func _updateTaskTx[ExtData ExtDataEntity](c context.Context, tx *gorm.DB, m Models, task *Task[ExtData], fsm FSM[ExtData]) error {
-	keyConflict, e := AddUnique(tx, m.UniqueRequestModel, task, false)
+func _updateTaskTx[Data DataEntity](c context.Context, tx *gorm.DB, m Models, task *Task[Data], fsm FSM[Data]) error {
+	keyConflict, e := AddUnique(tx, m, task, false)
 	if e != nil {
 		return e
 	}
@@ -123,7 +126,7 @@ func _updateTaskTx[ExtData ExtDataEntity](c context.Context, tx *gorm.DB, m Mode
 		return nil
 	}
 
-	var currentTask Task[ExtDataEntity]
+	var currentTask Task[Data]
 	currentTask.ID = task.ID
 	if e = tx.Table(m.TaskModel.TableName()).First(&currentTask).Error; e != nil {
 		return e
@@ -146,7 +149,7 @@ func _updateTaskTx[ExtData ExtDataEntity](c context.Context, tx *gorm.DB, m Mode
 		return nil
 	}
 
-	if e = UpdateExtData(c, tx, m.ExtDataModel, task); e != nil {
+	if e = UpdateData(c, tx, m, task); e != nil {
 		return e
 	}
 
@@ -157,27 +160,23 @@ func _updateTaskTx[ExtData ExtDataEntity](c context.Context, tx *gorm.DB, m Mode
 	return nil
 }
 
-func UpdateExtData[ExtData ExtDataEntity](c context.Context, tx *gorm.DB, extDataModel schema.Tabler, task *Task[ExtData]) error {
-	if task.GetExtData() == nil {
-		return nil
-	}
-
+func UpdateData[Data DataEntity](c context.Context, tx *gorm.DB, m Models, task *Task[Data]) error {
 	_query := func(_tx *gorm.DB) *gorm.DB {
-		return _tx.Table(extDataModel.TableName()).Where("task_id = ?", task.ID)
+		return _tx.Table(m.DataModel.TableName()).Where("task_id = ?", task.ID)
 	}
 
 	if len(task.SelectColumns) <= 0 {
-		if err := _query(tx).Omit(task.OmitColumns...).Updates(task.ExtData).Error; err != nil {
+		if err := _query(tx).Omit(task.OmitColumns...).Updates(task.GetData()).Error; err != nil {
 			return err
 		}
 		return nil
 	}
 
 	sqlStr1 := tx.ToSQL(func(tx *gorm.DB) *gorm.DB {
-		return _query(tx).Omit(task.OmitColumns...).Updates(task.ExtData)
+		return _query(tx).Omit(task.OmitColumns...).Updates(task.GetData())
 	})
 	sqlStr2 := tx.ToSQL(func(tx *gorm.DB) *gorm.DB {
-		return _query(tx).Select(task.SelectColumns).Updates(task.ExtData)
+		return _query(tx).Select(task.SelectColumns).Updates(task.GetData())
 	})
 
 	sql, err := util.MergeUpdateSQL(sqlStr1, sqlStr2)
