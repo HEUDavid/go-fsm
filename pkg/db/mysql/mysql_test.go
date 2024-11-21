1b583921d1f03df5d8aa668aa3cbdfd8fef0374e
David Xiang <admin@mdavid.cn>
2024-11-22 02:49:45 +0800

    upt

--- pkg/db/mysql/mysql_test.go
+++ pkg/db/mysql/mysql_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestInitDB(t *testing.T) {
-	factory := &Factory{Section: "mysql_tidb_aws_tokyo"}
+	factory := &Factory{Section: "mysql_aiven_do_blr"}
 	config := (*util.GetConfig())[factory.GetDBSection()].(util.Config)
 	if err := factory.InitDB(config); err != nil {
 		t.Error(err)
